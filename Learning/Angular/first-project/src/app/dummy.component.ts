7cd9465dd8d72c1f51a0185f6fe9f20158dc6b61
ragavan122 <vijayaragavansr2020@gmail.com>
2024-04-12 10:26:42 +0530

    first commit

---
+++ Learning/Angular/first-project/src/app/dummy.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    
+        selector: 'app-rummy',
+        templateUrl: './dummy.component.html',
+        styleUrl: './dummy.component.css'
+      
+
+})
+
+export class dummyComponent{
+
+title="my name is billa";
+
+}
---
+++ Learning/php/index.php
@@ -0,0 +1,23 @@
+<?php
+
+#    comment line in php
+
+//single comment line
+
+/*
+
+
+multiple comment line
+
+
+
+*/
+
+
+#
+
+
+
+
+
+?>
